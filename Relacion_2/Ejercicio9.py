b3c7cd7e0fc4f38e77e08b78d1393ba7ef60e699
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-10-07 14:37:53 +0200

    Ejercicio 9 completado

--- Relacion_2/Ejercicio9.py
+++ Relacion_2/Ejercicio9.py
@@ -7,3 +7,14 @@ anterior para que también funcione cuando el día o el mes se introduzcan con u
 solo carácter.
 
 '''
+
+fechaNacimiento = input("Dime la fecha en que naciste en el formato dd/mm/aa: ")
+
+dia = fechaNacimiento.split("/",2)[0]
+mes = fechaNacimiento.split("/",2)[1]
+año = fechaNacimiento.split("/",2)[2]
+
+if len(dia)<2: dia = "0" + dia
+if len(mes)<2: mes = "0" + mes
+
+print(f"El dia es {dia}, el mes {mes} y el año {año}.")
