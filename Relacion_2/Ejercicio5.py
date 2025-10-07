8d29ce712a637f1e1137d920913d16bd17825510
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-10-07 13:59:32 +0200

    Ejercicio 5 completado

--- Relacion_2/Ejercicio5.py
+++ Relacion_2/Ejercicio5.py
@@ -5,3 +5,14 @@ Escribir un programa que pida al usuario que introduzca una frase en la consola 
 muestre por pantalla la frase invertida.
 
 '''
+'''
+frase = input("Dime una frase aleatoria: ")
+fraseInv = ''.join(reversed(frase))
+print(fraseInv)
+
+'''
+#otra forma
+
+frase = input("Dime una frase aleatoria: ")
+fraseInv = frase[::-1]
+print(fraseInv)
