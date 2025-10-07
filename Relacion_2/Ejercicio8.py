255923e905cee26a1e18464030f6c2fee586493c
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-10-07 14:27:12 +0200

    Ejercicio 8 completado

--- Relacion_2/Ejercicio8.py
+++ Relacion_2/Ejercicio8.py
@@ -6,3 +6,15 @@ con dos decimales y muestre por pantalla el número de euros y el número de
 céntimos del precio introducido.
 
 '''
+
+'''
+precio = input("Dime el precio exacto de algo: ")
+precioEuros = precio.split(".")[0]
+precioCentimos = precio.split(".")[1]
+print(f"Son {precioEuros} euros y {precioCentimos} centimos.")
+
+'''
+#mas simple
+
+precio = input("Dime el precio exacto de algo: ")
+print(f"Son {precio.split(".")[0]} euros y {precio.split(".")[1]} centimos.")
