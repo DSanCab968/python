e8bd6eafff542769af3f806e3be049f97d2da9b8
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-10-08 09:44:24 +0200

    Ejercicio 11 completado

--- Relacion_2/Ejercicio11.py
+++ Relacion_2/Ejercicio11.py
@@ -7,3 +7,11 @@ de su precio unitario con 6 dígitos enteros y 2 decimales, el número de unidad
 tres dígitos y el coste total con 8 dígitos enteros y 2 decimales.
 
 '''
+
+producto = input("Dime un producto: ")
+precio = float(input("Dime el precio: "))
+unidades = int(input("Dime cuantos quieres: "))
+total = unidades*precio
+
+
+print(f"{producto}: {precio:08.2f} x {unidades:03d} = {total:010.2f}")
