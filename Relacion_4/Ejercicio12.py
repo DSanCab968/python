a598f65e1598a062aaceb3ae14bb393be95f3083
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-10-28 13:48:44 +0100

    ejercicio 12 acabado

--- Relacion_4/Ejercicio12.py
+++ Relacion_4/Ejercicio12.py
@@ -6,3 +6,13 @@ Escribir un programa en el que se pregunte al usuario por una frase y una letra,
 muestre por pantalla el número de veces que aparece la letra en la frase.
 
 '''
+
+frase = input("Dime un frase: ")
+letra = input("Dime una letra: ")
+
+numLetras = frase.count(letra)
+
+print(numLetras)
+
+
+
