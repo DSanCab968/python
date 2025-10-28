3c7194974dfde4634ce5caf897709b07bf0bf9ff
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-10-28 13:36:27 +0100

    ejercicio 11 hecho

--- Relacion_4/Ejercicio11.py
+++ Relacion_4/Ejercicio11.py
@@ -6,3 +6,11 @@ Escribir un programa que pida al usuario una palabra y luego muestre por pantall
 una a una las letras de la palabra introducida empezando por la última.
 
 '''
+
+palabra = input("Introduce una palabra: ")
+
+for letra in palabra[::-1]:#basicamente indicamos toda la cadena y que la recorra con paso inverso (dcha a izda)
+    print(letra)
+
+
+
