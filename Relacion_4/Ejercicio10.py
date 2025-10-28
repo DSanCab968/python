8a24b7276045551d4f763c3578eee011d985a658
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-10-28 13:31:40 +0100

    ej 10 ccompletado

--- Relacion_4/Ejercicio10.py
+++ Relacion_4/Ejercicio10.py
@@ -6,3 +6,18 @@ Escribir un programa que pida al usuario un número entero y muestre por pantall
 es un número primo o no.
 
 '''
+
+num = int(input("Introduce un número entero: "))
+
+if num <= 1:
+    print(f"{num} no es primo.")
+else:
+    es_primo = True
+    for i in range(2, int(num**0.5) + 1):#lo de la raiz cuadrada
+        if num % i == 0:
+            es_primo = False
+            break
+    if es_primo:
+        print(f"{num} es primo.")
+    else:
+        print(f"{num} no es primo.")
