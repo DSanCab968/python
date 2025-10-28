74febc8e710d2a575e6c2264b9ae75d6c7988ebc
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-10-28 13:21:12 +0100

    Ejercicio 8 completado

--- Relacion_4/Ejercicio8.py
+++ Relacion_4/Ejercicio8.py
@@ -11,7 +11,48 @@ un triángulo rectángulo como el de más abajo.
 9 7 5 3 1
 
 '''
-num = abs(int(input("Dime un numero: ")))
+#con 2 for
+
+numOcurr = abs(int(input("Dime un número: ")))
+
+for i in range(1, numOcurr + 1):
+    for j in range(i, 0, -1):
+        print(2*j - 1, end=" ")
+    print()
+
+'''
+#con for y array
+
+numOcurr = abs(int(input("Dime un número: ")))
+
+for i in range(1, numOcurr + 1):
+    fila = [] 
+    for j in range(i, 0, -1):
+        fila.append(2*j - 1)
+    print(fila)
+'''
+    
+
+
+'''
+Forma incompleta:
+
+numOcurr = abs(int(input("Dime un numero: ")))
+
+x = 1
+arr=[]
+
+for i in range(0,numOcurr):
+    arr.append(x)
+    arr.reverse()
+
+    print(arr)
+    x += 2
+'''
+
+
+
+
 
 
     
