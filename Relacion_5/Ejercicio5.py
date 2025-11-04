5c53cbcedf25a64ee57f4105554fb268c74a7e67
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-11-04 14:37:23 +0100

    seguir por ej 5 (sin acab)

--- Relacion_5/Ejercicio5.py
+++ Relacion_5/Ejercicio5.py
@@ -6,3 +6,14 @@ Escribir un programa que almacene en una lista los números del 1 al 10 y los
 muestre por pantalla en orden inverso separados por comas
 
 '''
+
+numeros = []
+i=1
+
+while i <= 10:
+    numeros.append(str(i))
+    i+=1
+
+#(acabar)
+
+
