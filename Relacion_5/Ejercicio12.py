bcd7b0c0fb2a2862b44a4543defc88e56a5d4566
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-11-04 12:55:47 +0100

    creacion ejs rel 5

---
+++ Relacion_5/Ejercicio12.py
@@ -0,0 +1,14 @@
+'''
+
+Ejercicio 12
+
+Escribir un programa que almacene las matrices
+en una lista y muestre por pantalla su producto.
+Nota: Para representar matrices mediante listas usar listas anidadas, representando
+cada vector fila en una lista
+
+'''
+
+
+
+
---
+++ Relacion_5/Ejercicio8.py
@@ -0,0 +1,15 @@
+'''
+
+Ejercicio 8
+
+Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un
+palíndromo
+
+'''
+
+
+
+
+
+
+    
