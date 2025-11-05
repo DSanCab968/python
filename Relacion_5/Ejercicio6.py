237e6108f26ac46e4ccbb356d4936f7e0cba6d13
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-11-05 09:53:59 +0100

    ejercicio 6 completado

--- Relacion_5/Ejercicio6.py
+++ Relacion_5/Ejercicio6.py
@@ -10,5 +10,23 @@ usuario tiene que repetir
 
 '''
 
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+notas = []
+
+for asignatura in asignaturas:
+    nota = float(input(f"Dime la nota que sacaste en {asignatura}: "))
+    notas.append(nota)
+
+suspensas = []
+for i in range(len(asignaturas)):
+    if notas[i] < 5:
+        suspensas.append(asignaturas[i])
+
+if suspensas:
+    print("Tienes que repetir las siguientes asignaturas:")
+    for materia in suspensas:
+        print(materia)
+else:
+    print("Has aprobado todo")
 
 
