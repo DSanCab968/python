498cb2c63477cb85babee7cadff9ed93f2ea935b
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-12-02 13:22:16 +0100

    ejercicio 9 rel 5

--- Relacion_5/Ejercicio9.py
+++ Relacion_5/Ejercicio9.py
@@ -6,3 +6,12 @@ Escribir un programa que pida al usuario una palabra y muestre por pantalla el
 número de veces que contiene cada vocal
 
 '''
+palabra = input("Dime una palabra: ")
+
+a = palabra.count("a")
+e = palabra.count("e")
+i = palabra.count("i")
+o = palabra.count("o")
+u = palabra.count("u")
+
+print(f"A: {a} | E: {e} | I: {i} | O: {o} | U: {u}")
