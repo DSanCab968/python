406b229ef6e4e5786e459cb383b968144f122f6f
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-12-02 13:26:10 +0100

    ejercicio 11 rel 5

--- Relacion_5/Ejercicio11.py
+++ Relacion_5/Ejercicio11.py
@@ -7,6 +7,11 @@ muestre por pantalla su producto escalar
 
 '''
 
+v1 = [1,2,3]
+v2 = [-1,0,2]
 
+producto = (v1[0]*v2[0])+(v1[1]*v2[1])+(v1[2]*v2[2])
+
+print(f"El producto escalar de v1 y v2 es {producto}.")
 
 
