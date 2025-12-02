e3ef6eb52406b0d4f092ea57254ad83ec085ae30
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-12-02 13:21:45 +0100

    ejercicio 8 rel 5

--- Relacion_5/Ejercicio8.py
+++ Relacion_5/Ejercicio8.py
@@ -7,6 +7,33 @@ palíndromo
 
 '''
 
+palabra = input("Dime una palabra: ")
+inversa = []
+
+for letra in palabra:
+    inversa.insert(0, letra)
+
+inversa = "".join(inversa)
+
+if palabra == inversa:
+    print(f"La palabra {palabra} es un palindromo")
+else:
+    print(f"La palabra {palabra} no es un palindromo")
+
+
+'''
+Otra forma:
+
+
+
+palabra = input("Dime una palabra: ")
+
+if palabra == palabra[::-1]:
+    print(f"La palabra {palabra} es un palindromo")
+else:
+    print(f"La palabra {palabra} no es un palindromo")
+
+'''
 
 
 
