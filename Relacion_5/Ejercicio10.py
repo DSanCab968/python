900389f8b9f8c211064997583890fbf4d8ebabd7
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-12-02 13:22:27 +0100

    ejercicio 10 rel 5

--- Relacion_5/Ejercicio10.py
+++ Relacion_5/Ejercicio10.py
@@ -6,3 +6,9 @@ Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 4
 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios
 
 '''
+precios=[50,75,46,22,80,65,8]
+
+mayor = max(precios)
+menor = min(precios)
+
+print(f"El precio mas alto es {mayor} y el mas bajo {menor}")
