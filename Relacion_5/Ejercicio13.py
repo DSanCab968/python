2ce165199e56bf537636c32b450a900cb3273fd4
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-12-02 13:48:09 +0100

    ej 13 rel 5

--- Relacion_5/Ejercicio13.py
+++ Relacion_5/Ejercicio13.py
@@ -6,4 +6,17 @@ Escribir un programa que pregunte por una muestra de números, separados por
 comas, los guarde en una lista y muestre por pantalla su media y desviación típica.
 
 '''
+lista = []
 
+x = input("Escribe numeros separados por comas: ")
+
+
+for n in x.split(","):
+    lista.append(float(n))
+
+media = sum(lista)/len(lista)
+
+varianza = sum((x - media) ** 2 for x in lista) / len(lista)
+desviacion = varianza ** 0.5
+
+print(f"La media es {media} y la desviacion tipica {desviacion}")
