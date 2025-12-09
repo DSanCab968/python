76847046127da90fa87f1ae26a627e54e9c36903
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-12-09 14:32:04 +0100

    ej 12 rel 5 acabado

--- Relacion_5/Ejercicio12.py
+++ Relacion_5/Ejercicio12.py
@@ -9,6 +9,35 @@ cada vector fila en una lista
 
 '''
 
+a = (
+    (1, 2, 3),
+    (4, 5, 6)
+)
+
+b = (
+    (-1, 0),
+    (0, 1),
+    (1, 1)
+)
+
+c = [
+    [0,0],
+    [0,0]
+]
+
+
+for i in range(len(a)):# es 2
+    for j in range(len(b[0])):#cojo las columnas de b q son 2
+        for k in range(len(b)): # columnas de a entre filas de b
+            c[i][j] += a[i][k] * b[k][j]
+        
+print(c)
+
+        
+
+
+
+
 
 
 
