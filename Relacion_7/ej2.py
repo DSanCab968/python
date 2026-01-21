9df8c08e643db320b5e9d07317324c189d98afcd
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2026-01-21 09:36:20 +0100

    ej 2 rel 7 acabado

---
+++ Relacion_7/ej2.py
@@ -0,0 +1,13 @@
+'''
+Ejercicio 2
+
+Escribir una función a la que se le pase una cadena <nombre> y muestre por
+pantalla el saludo ¡hola <nombre>!.
+
+'''
+
+def saludo2(nombre):
+    print(f"Hola {nombre}")
+
+n=input("Dime tu nombre: ")
+saludo2(n)
